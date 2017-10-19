d16f1877a0a93b7726274c3dcaf586fd6cd786fa
kento-iuchi <31495048+kento-iuchi@users.noreply.github.com>
2017-10-19 10:26:48 +0900

    first commit

---
+++ app/Model/Item.php
@@ -0,0 +1,11 @@
+<?php
+class Item extends AppModel {
+    public $validate = array(
+        'title' => array(
+            'rule' => 'notBlank'
+        ),
+        'body' => array(
+            'rule' => 'notBlank'
+        )
+    );
+}
