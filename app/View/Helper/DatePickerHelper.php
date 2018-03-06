6971a60e513f04de945cf9d871dcbe552e5398b4
DESKTOP-ADB9V2G\itou <kento.iuchi@e-grant.net>
2018-03-06 12:02:19 +0900

    jqueryのパスが間違っていたのを修正

--- app/View/Helper/DatePickerHelper.php
+++ app/View/Helper/DatePickerHelper.php
@@ -6,10 +6,9 @@ var $helpers = array("Form", "Html");
 
 function datepicker($fieldName, $options = array()){
     //外部ファイル
-    $ext = $this->Html->script('jquery-3.2.1.js', array('inline' => false))
+    $ext = $this->Html->script('jquery-3.2.1.min.js', array('inline' => false))
         . $this->Html->script('jquery-ui.js', array('inline' => false))
         . $this->Html->css('jquery-ui.css', null, array('inline' => false));
-        // . $this->Html->script('jquery.ui.datepicker-ja', array('inline' => false))
 
     //テキストボックスのhtml
     $ext .= $this->Form->input($fieldName, $options);
