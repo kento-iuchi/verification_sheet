055a1628de865812ada4d3c6e5af7c0a87c54155
DESKTOP-ADB9V2G\itou <kento.iuchi@e-grant.net>
2018-03-06 17:01:21 +0900

    modifiedが更新されていなかったのを修正

--- app/Model/Item.php
+++ app/Model/Item.php
@@ -28,6 +28,16 @@ class Item extends AppModel
         return true;
     }
 
+    public function save($data = null, $validate = true, $fieldList = array()) {
+        // 保存前に modifiedフィールドをクリア
+        // modifiedが更新されるようにする
+        $this->set($data);
+        if (isset($this->data[$this->alias]['modified'])) {
+            unset($this->data[$this->alias]['modified']);
+        }
+        return parent::save($this->data, $validate, $fieldList);
+    }
+
 
     public function formatDate($date)
     {
