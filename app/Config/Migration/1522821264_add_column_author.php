07afe222c77952f8175413a3e711fa89ed2d1b1b
DESKTOP-ADB9V2G\itou <kento.iuchi@e-grant.net>
2018-04-04 15:43:36 +0900

    auhorカラムを追加

---
+++ app/Config/Migration/1522821264_add_column_author.php
@@ -0,0 +1,50 @@
+<?php
+class AddColumnAuthor extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'add_column_author';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+			'create_field' => array(
+				'items' => array(
+					'author' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'response_to_confirm_comment'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_field' => array(
+				'items' => array('author'),
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}
