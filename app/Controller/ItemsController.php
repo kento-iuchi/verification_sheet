6901daa6701242aca818cd8d046bd5ccf6663264
DESKTOP-ADB9V2G\itou <kento.iuchi@e-grant.net>
2018-04-12 16:55:01 +0900

    てｓｔ

--- app/Controller/ItemsController.php
+++ app/Controller/ItemsController.php
@@ -171,7 +171,7 @@ class ItemsController extends AppController
 
     public function retrieve_github_push()
     {
-        $this->log('pushてすと10');
+        $this->log('pushてすと11');
         echo 'post successed';
 
         $this->autoRender = false;
