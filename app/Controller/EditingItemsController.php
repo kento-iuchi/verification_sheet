869744794370ece3e773e6761f1a19ffa1edc75a
DESKTOP-ADB9V2G\itou <kento.iuchi@e-grant.net>
2018-07-13 11:44:22 +0900

    不要なデバッグ分の削除

--- app/Controller/EditingItemsController.php
+++ app/Controller/EditingItemsController.php
@@ -8,9 +8,7 @@ class EditingItemsController extends AppController
     {
         $current_datetime = new Datetime(date("y-m-d H:i:s"));
         $current_datetime = $current_datetime->getTimestamp();
-        debug($current_datetime);
         $ids_and_their_modified = Hash::combine($this->EditingItem->find('all'), '{n}.EditingItem.id', '{n}.EditingItem.modified');
-        debug($ids_and_their_modified);
         if (empty($ids_and_their_modified)) {
             return false;
         }
@@ -18,7 +16,6 @@ class EditingItemsController extends AppController
         foreach ($ids_and_their_modified as $id => $modified) {
             $modified = new Datetime($modified);
             $modified = $modified->getTimestamp();
-            debug($modified);
 
             if (($current_datetime - $modified) > 1080) {
                 if ($this->EditingItem->delete($id)){
