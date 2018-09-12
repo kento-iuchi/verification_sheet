b9a77ac631d45d26621cb0bf8e4f32d26d16a33d
DESKTOP-ADB9V2G\itou <kento.iuchi@e-grant.net>
2018-09-12 16:44:00 +0900

    次回リリース日のアップデート失敗時にログを残すように

--- app/Controller/SystemVariablesController.php
+++ app/Controller/SystemVariablesController.php
@@ -17,6 +17,7 @@ class SystemVariablesController extends AppController
             if ($this->SystemVariable->isValidDateFormat($this->request->data['next_release_date'])) {
                 $save_data['SystemVariable']['next_release_date'] = $this->request->data['next_release_date'];
                 if ($this->SystemVariable->save($save_data)) {
+                    $this->log("next release date saved [" . $save_data['SystemVariable']['next_release_date']. "]");
                     return true;
                 } else {
                     throw new Exception("saving next release date was failed");
@@ -26,7 +27,7 @@ class SystemVariablesController extends AppController
             }
         } catch(Exception $e){
             header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
-            echo "error：" . $e->getMessage();
+            $this->log("error：" . $e->getMessage());
             exit;
         }
     }
